9d29fd5b38dd7481b3b0e9af0260a4293ac53375
s-kugan <42327531+s-kugan@users.noreply.github.com>
2023-09-07 13:19:49 -0400

    code started to get unique elements in an array

---
+++ uniarray.js
@@ -0,0 +1,24 @@
+/****************************************************************************************************
+ * This code will remove the duplicate elements in an array and return an array with unique elements
+****************************************************************************************************/
+
+
+
+let uniqarray = (duparray)=>{
+
+    let uniqarray = [];
+
+    for (let i=0;i<duparray.length;i++){
+        if (indexOf(duparray[i])==-1){
+            uniqarray.push(duparray[i])
+        }
+    }
+
+    console.log('uniqarray***',uniqarray);
+
+
+}
+
+
+
+uniqarray([1,4,6,1,9,3,5,10,9,23,4]);
