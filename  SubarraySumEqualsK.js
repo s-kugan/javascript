04bab98aeb6688f898162dbb3a3ee8534b037629
s-kugan <42327531+s-kugan@users.noreply.github.com>
2023-09-20 12:43:14 -0400

    question added

---
+++  SubarraySumEqualsK.js
@@ -0,0 +1,15 @@
+/******************************************************************************************************************
+ Given an unsorted array of integers, find the number of subarrays having a sum exactly equal to a given number k.
+
+Examples: 
+
+Input : arr[] = {10, 2, -2, -20, 10}, k = -10
+Output : 3
+Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
+
+Input : arr[] = {9, 4, 20, 3, 10, 5}, k = 33
+Output : 2
+Explanation: Subarrays : arr[0…2], arr[2…4] have a sum exactly equal to 33.
+****************************************************************************/
+
+
