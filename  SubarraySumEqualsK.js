e41a1d580a80d26d4d36f7a32caa9493cc78b71b
s-kugan <42327531+s-kugan@users.noreply.github.com>
2023-09-20 13:36:58 -0400

    design added

---  SubarraySumEqualsK.js
+++  SubarraySumEqualsK.js
@@ -12,4 +12,22 @@ Output : 2
 Explanation: Subarrays : arr[0…2], arr[2…4] have a sum exactly equal to 33.
 ****************************************************************************/
 
+let arr = [10, 2, -2, -20, 10];
+let k = -10;
+let sum = 0;
+let newarray = [];
+let no;
 
+const findsubarray = () => {
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+
+    if (sum == k) {
+      newarray.push[arr[i]];
+    }
+  }
+
+  console.log(newarray.length);
+};
+
+findsubarray();
