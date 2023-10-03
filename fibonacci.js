77759521129ae981af835b4fd5f6dd9a41bdecc1
s-kugan <42327531+s-kugan@users.noreply.github.com>
2023-10-03 10:11:38 -0400

    Recursive approach for Fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+/*************Recursive approach to find and print the Nth number in Fibonacci series************************/
+
+const Fibonacci = (n) => {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  } else return Fibonacci(n - 1) - Fibonacci(n + 1);
+};
