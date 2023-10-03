ee5b307b602bcc054c7fb388d5152b0b7ae442f4
s-kugan <42327531+s-kugan@users.noreply.github.com>
2023-10-03 11:16:00 -0400

    print Fibonacci series up to nth number

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,30 @@
 /*************Recursive approach to find and print the Nth number in Fibonacci series************************/
+//Fn = F(n-1) +F(n-2);
+//[0,1,1,]
 
 const Fibonacci = (n) => {
-  if (n == 0) {
+  if (n == 1) {
     return 0;
   }
-  if (n == 1) {
+  if (n == 2) {
     return 1;
-  } else return Fibonacci(n - 1) - Fibonacci(n + 1);
+  } else return Fibonacci(n - 1) + Fibonacci(n - 2);
 };
 
+console.log(8 + "th fibonacci number is " + Fibonacci(8));
 
-console.log(Fibonacci(5));
+/***************print Fibonacci series up to nth number***************/
+const printFibonacciSeries = (n) => {
+  let n1 = 0;
+  let n2 = 1;
+  let nextnum;
+
+  for (let i = 1; i <= n; i++) {
+    console.log(n1);
+    nextnum = n1 + n2;
+    n1 = n2;
+    n2 = nextnum;
+  }
+};
+
+printFibonacciSeries(5);
