fcadca556e8f25b0ba71baf896de21b2521a0281
s-kugan <42327531+s-kugan@users.noreply.github.com>
2023-10-23 22:31:16 -0400

    TwoSum Implementation provided

--- TwoSum.js
+++ TwoSum.js
@@ -32,3 +32,17 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 
 */
+
+let twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == tar4) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+
+console.log(twoSum([3, 2, 4], 6));
